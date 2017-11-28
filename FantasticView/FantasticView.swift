d3016e753b32ea752b478567aa046f138585e465
HongpengYu <480814177@qq.com>
2017-11-29 00:22:21 +0800

    修改代码

--- FantasticView/FantasticView.swift
+++ FantasticView/FantasticView.swift
@@ -8,22 +8,21 @@
 
 import UIKit
 
+@available(iOS 10.0, *)
 public class FantasticView: UIView {
     let colors: [UIColor] = [.red, .orange, .yellow, .purple, .blue]
     var colorCount = 0
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        if #available(iOS 10.0, *) {
-            let colorChange = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in
-                UIView.animate(withDuration: 2.0, animations: {
-                    self.layer.backgroundColor = self.colors[self.colorCount % 5].cgColor
-                    self.colorCount += 1
-                })
-            }
-            
-            colorChange.fire()
+        let colorChange = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in
+            UIView.animate(withDuration: 2.0, animations: {
+                self.layer.backgroundColor = self.colors[self.colorCount % 5].cgColor
+                self.colorCount += 1
+            })
         }
+        
+        colorChange.fire()
     }
     
     required public init?(coder aDecoder: NSCoder) {
